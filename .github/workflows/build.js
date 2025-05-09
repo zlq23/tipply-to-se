15225e91029edbbe9b6cc6e9fa7b401c487ad323
Zalech23 <145216942+zlq23@users.noreply.github.com>
2025-05-09 15:53:02 +0200

    Update build.js

--- .github/workflows/build.js
+++ .github/workflows/build.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
 
+const html = fs.readFileSync('index.html', 'utf8');
+const css = fs.readFileSync('style.css', 'utf8');
+const js = fs.readFileSync('script.js', 'utf8');
+
+const final = html
+  .replace(
+    /<link rel="stylesheet" href="style.css">/,
+    `<style>\n${css}\n</style>`
+  )
+  .replace(
+    /<script src="script.js"><\/script>/,
+    `<script>\n${js}\n</script>`
+  );
+
+fs.writeFileSync('tipply-to-se.html', final);
